c5ab4acd34bd284932cbc6363d0f64447dcd13c8
Joe Pettersson <joe8bit@gmail.com>
2015-04-17 14:24:03 +0100

    make login-validation less complex

--- src/app/main/login-validation.service.js
+++ src/app/main/login-validation.service.js
@@ -4,36 +4,55 @@ angular.module('boilerplate')
   .service('LoginValidationSrv', function LoginValidationSrv() {
 
     /**
+     * A canned set of responses
+     * @type {Object}
+     */
+    var messages = {
+      user: 'You must enter a username and password',
+      username: 'You must enter a username',
+      password: 'You must enter a password'
+    };
+
+    /**
+     * Checks the existence of the user object
+     * @param  {Object} user The user object
+     * @return {Boolean}      Whether it is valid or not
+     */
+    function validateUserObject(user) {
+      return !!user;
+    }
+
+    /**
+     * Checks the validity of the username field
+     * @param  {Object} user The user object
+     * @return {Boolean}      Whether it is valid or not
+     */
+    function validateUsername(user) {
+      return user && !!user.username;
+    }
+
+    /**
+     * Checks the validity of the password field
+     * @param  {Object} user The user object
+     * @return {Boolean}      Whether it is valid or not
+     */
+    function validatePassword(user) {
+      return user && !!user.password;
+    }
+
+    /**
      * Performs some very naive validation of properties on an object
      * @param  {Object} user The user object to validate
      * @return {Object}      A validation object
      */
-    var _validate = function(user) {
-      if (!user) {
-        return {
-          valid: false,
-          message: 'You must enter a username and password'
-        };
-      }
-
-      if (!user.username) {
-        return {
-          valid: false,
-          message: 'You must enter a username'
-        };
-      }
-
-      if (!user.password) {
-        return {
-          valid: false,
-          message: 'You must enter a password'
-        };
-      }
-
-      return {
-        valid: true
-      };
-    };
+    function _validate(user) {
+      return _.chain([[validateUserObject, 'user'], [validateUsername, 'username'], [validatePassword, 'password']])
+              .map(function(validator) {
+                return (!validator[0](user)) ? { valid: false, message: messages[validator[1]] } : { valid: true };
+              })
+              .find({valid: false})
+              .value();
+    }
 
     return {
       validate: _validate
